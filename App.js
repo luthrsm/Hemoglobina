477b688f79d2a5bb5e69a8f21e73efc3d7afcf47
nicolas santos <nicosantosmaia18@gmail.com>
2024-07-31 21:15:15 -0300

    telas de boas vindas

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from './src/pagBemVindo'
+import { View, Text, TouchableOpacity, Image, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import AnimatedPaginationDot from 'react-native-animated-pagination-dot';
+import * as Font from 'expo-font'
+
+const Stack = createNativeStackNavigator();
+
+
+
 
 export default function App() {
+
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+  if (!fontsLoaded) {
+    return console.log('fontes carregadas'); // ou uma tela de carregamento
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eef',
     alignItems: 'center',
     justifyContent: 'center',
   },
