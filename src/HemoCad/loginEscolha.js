e5140b167fee2b9070c959d8a7fe301f434c957f
Gabrielly <116506930+FoxyGabys@users.noreply.github.com>
2024-09-09 19:16:40 -0300

    Telas do Cadastro Hemocentro e Home

---
+++ src/HemoCad/loginEscolha.js
@@ -0,0 +1,95 @@
+import { Text, SafeAreaView, StyleSheet, View, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
+
+const LoginEscolha = () => {
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <View style={styles.tudoJunto}>
+                <Image style={styles.logo} source={require('../../assets/img/hemoCadImages/logoHemoglobina.png')} />
+
+                <View style={styles.dentroCad}>
+
+                    <Text style={styles.textoBranco1}>Realize o seu cadastro</Text>
+
+                    <View styles={styles.ordemTam}>
+                        <Text style={styles.textoBranco2}>E faça parte de uma comunidade de doadores e hemocentros</Text>
+
+                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('LoginHemocentro')} >
+
+                            <Text style={styles.buttonText}>Hemocentro</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('LoginDoador')} >
+                            <Text style={styles.buttonText}>Doador</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+
+        </SafeAreaView>
+    );
+}
+
+export default LoginEscolha;
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 8,
+      width: '100%',
+      alignItems: 'center',
+      backgroundColor: '#ffff',
+    },
+    tudoJunto: {
+      top: 100,
+      alignItems: 'center',
+    },
+    textoBranco1: {
+      fontSize: 28,
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    textoBranco2: {
+      fontSize: 17,
+      marginTop: 20,
+      color: 'white',
+      marginBottom: 20,
+      textAlign: 'center',
+    },
+    ordemTam: {
+      width: 250,
+    },
+    button: {
+      height: 70,
+      width: 250,
+      marginBottom: 25,
+      borderRadius: 10,
+      alignSelf: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#EEF0EB',
+    },
+    buttonText: {
+      fontSize: 20,
+      color: '#000',
+      textAlign: 'center',
+    },
+    dentroCad: {
+      top: 70,
+      padding: 20,
+      width: '100%',
+      color: 'white',
+      borderRadius: 20,
+      alignItems: 'center',
+      backgroundColor: '#AF2B2B',
+    },
+    logo: {
+      width: 80,
+      height: 80,
+    },
+});
---
+++ src/HomeHemocentro/homeHemocentro.js
@@ -0,0 +1,125 @@
+import { Text, SafeAreaView, View, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome6 } from '@expo/vector-icons';
+
+
+import MenuHemo from '../../components/elementosHemocentro/menuHemo';
+
+
+const HomeDoador = () => {
+
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.headerContainer}>
+                <Text style={styles.title}> Bem vindo, hemocentro!</Text>
+            </View>
+
+            <View style={styles.mainContainer}>
+
+                  <View style={styles.txtFotoContainer}>
+                    <Text style={styles.txtTitle}>Gerencie o nível do banco de sangue do seu hemocentro</Text>
+                    <View style={styles.fotoCont}>
+                        <Image style={styles.image} source={require('../../assets/img/hemoCadImages/grafico.png')} />
+                    </View>
+                </View>
+ 
+                <View style={styles.doacoesContainer}>
+                    <Text style={styles.doacoesTxt}>Adicionar nova doação:</Text>
+                    <TouchableOpacity style={styles.botaoAdDoacao} onPress={() => navigation.navigate('AdiocionarDoahemo')}>
+                        <Text style={styles.txtBotao}>Adicionar nova doação</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.botaoAdDoacao}>
+                        <Text style={styles.txtBotao}>Gerenciar banco de sangue</Text>
+                    </TouchableOpacity>
+
+                </View>
+            </View>
+
+            <MenuHemo />
+        </View>
+
+    )
+}
+
+export default HomeDoador;
+
+//Cores do App
+//#AF2B2B Vermelho principal
+//#EEF0EB branco do funco e dos textos
+//#000000 preto
+//#7A0000 vinho
+//#326771 azul 
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FDFEFF',
+      justifyContent: 'flex-end',
+    },
+    headerContainer: {
+      backgroundColor: '#AF2B2B',
+      height: '9%',
+      borderBottomLeftRadius: 8,
+      borderBottomRightRadius: 8,
+      padding: 10,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between"
+    },
+    title: {
+      color: '#EEF0EB',
+      marginTop: 7,
+      fontFamily: 'DM-Sans',
+      letterSpacing: 1.5
+    },
+    mainContainer: {
+      padding: 32,
+      top: 20,
+    },
+
+    txtTitle: {
+      fontFamily: 'Poppins-SemiBold',
+      fontSize: 18,
+    },
+
+    txtFotoContainer: {
+      marginBottom: 90,
+    },
+    txtBotao: {
+      color: '#EEF0EB',
+      fontFamily: 'DM-Sans',
+    },
+    image: {
+      width: '90%',
+      height: 300,
+    },
+    fotoCont: {
+      alignItems: 'center',
+      width: '100%',
+      height: 200,
+      marginBottom: 30,
+      marginTop: 20,
+    },
+    doacoesContainer: {
+      marginTop: 30,
+    },
+    doacoesTxt: {
+      fontFamily: 'Poppins-SemiBold',
+      fontSize: 17,
+      color: '#470404',
+      marginBottom: 30,
+      fontWeight: 500,
+    },
+    botaoAdDoacao:{
+      backgroundColor: '#1E6370',
+      height: 37,
+      width: 235,
+      borderRadius: 12,
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignSelf: 'center',
+      marginBottom: 30,
+    }
+});
