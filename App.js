8f9508b34b5b1def3a77316b0e553bbff628b98e
nicolas <nicosantosmaia18@gmail.com>
2024-10-13 15:26:39 -0300

    mapa dos hemocentros próximos

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Font from 'expo-font'
+
+const Stack = createNativeStackNavigator();
+
+
+import HemocentrosMap from './src/mapDoador';
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+        'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+        'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+        'DM-Sans': require('./assets/fonts/DMSans-VariableFont_opsz,wght.ttf'),
+        'DM-Sans Bold': require('./assets/fonts/DMSans-Bold.ttf'),
+        'DM-Sans Medium': require('./assets/fonts/DMSans-Medium.ttf'),
+
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []); if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen component={HemocentrosMap} name="HemocentrosMap"/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
