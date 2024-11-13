8c2aa939b79bf6dba9b81bbdd099fbd435fcc1b3
Gabrielly <116506930+FoxyGabys@users.noreply.github.com>
2024-11-13 18:35:53 -0300

    Update faleConoscoHemoConfig.js

--- src/Screen/hemocentro/ConfigHemo/faleConoscoHemoConfig.js
+++ src/Screen/hemocentro/ConfigHemo/faleConoscoHemoConfig.js
@@ -119,7 +119,8 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     marginTop: 7,
     fontFamily: 'DM-Sans',
-    letterSpacing: 1.5
+    letterSpacing: 1.5,
+    fontSize: 16,
   },
   mainContainer: {
     padding: 32,
